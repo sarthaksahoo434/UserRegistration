9f873e77e5bfe28fcccdcf8cbe8c0a82a543b753
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-20 00:33:07 +0530

    [sarthak]ADD:UserRegistration

---
+++ UC7_USERREGT/UC7_USERREGT/ValidateUserDetail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC7_USERREGT
+{
+  public  class ValidateUserDetail
+    {
+        public const string regexFirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string regexLastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public const string Regex_MobileNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
+        public const string Regex_Password = "^(?=.*[A-Z][a-z0-9@#.]).{8,}$";
+        public bool validateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, regexFirstName);
+
+        }
+        public bool validateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, regexLastName);
+        }
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, Regex_Email);
+        }
+        public bool ValidateMobile(string mobileNumber)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(mobileNumber, Regex_MobileNumber);
+        }
+        public bool ValidatePassword(string password)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
