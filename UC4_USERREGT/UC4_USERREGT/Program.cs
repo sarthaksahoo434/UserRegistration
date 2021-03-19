236e910fc2544bfc470e5644c06fbd5cf92000d7
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-20 00:25:59 +0530

    [sarthak]ADD:UserRegistration

---
+++ UC4_USERREGT/UC4_USERREGT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC4_USERREGT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        ValidateUserDetail regexValidation = new ValidateUserDetail();
+        Console.WriteLine("Enter Your First Name");
+            string FirstName = Console.ReadLine();
+        Console.WriteLine(regexValidation.validateFirstName(FirstName));
+            Console.WriteLine("Enter Your Last Name");
+            string LastName = Console.ReadLine();
+        Console.WriteLine(regexValidation.validateLastName(LastName));
+            Console.WriteLine("Enter Your Email Address");
+            string email = Console.ReadLine();
+        Console.WriteLine(regexValidation.ValidateEmail(email));
+            Console.WriteLine("Enter Your Mobile Number");
+            string mobileNumber = Console.ReadLine();
+            Console.WriteLine(regexValidation.ValidateMobile(mobileNumber));
+        }
+}
+}
---
+++ UC4_USERREGT/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UC4_USERREGT;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Arrange
+        ValidateUserDetail validateUserDetails = new ValidateUserDetail();
+        bool expectedResult = true;
+
+        [TestMethod]
+        public void Test_First_Name()
+        {
+            //Act        
+            bool result = validateUserDetails.validateFirstName("Sarthak");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+
+        public void Test_Last_Name()
+        {
+            //Act
+            bool result = validateUserDetails.validateLastName("Sahoo");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+
+        public void Test_Email()
+        {
+            //Act
+            bool result = validateUserDetails.ValidateEmail("cdf.xyz@bl.co.in");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_Phone_Number()
+        {
+            //Act
+            bool result = validateUserDetails.ValidateMobile("91 8917219415");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
