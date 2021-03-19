c97752df26a156f775d38fcc4e312a25616e8031
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-20 00:28:17 +0530

    [sarthak]ADD:UserRegistration

---
+++ UC5_USERREGT/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,66 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UC5_USERREGT;
+namespace UnitTestProject1
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        //Arrange
+        ValidateUserDetail validateUserDetails = new ValidateUserDetail();
+        bool expectedResult = true;
+
+        [TestMethod]
+        public void Test_First_Name()
+        {
+            //Act        
+            bool result = validateUserDetails.validateFirstName("Sarthak");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+
+        public void Test_Last_Name()
+        {
+            //Act
+            bool result = validateUserDetails.validateLastName("Sahoo");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+
+        public void Test_Email()
+        {
+            //Act
+            bool result = validateUserDetails.ValidateEmail("cdf.xyz@bl.co.in");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Test_Phone_Number()
+        {
+            //Act
+            bool result = validateUserDetails.ValidateMobile("91 8917219415");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        public void Test_Password()
+        {
+            //Act
+            bool result = validateUserDetails.ValidatePassword("Sarthak1");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+    }
+}
+
